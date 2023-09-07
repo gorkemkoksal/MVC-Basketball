6e89e6bee45019a709fd0a27ab96158080477cda
GorkemKoksal <gorkem.asrin.koksal@gmail.com>
2023-09-07 19:58:49 +0300

    validation

--- BasketballMVC/BasketballMVC/Models/Raptors.cs
+++ BasketballMVC/BasketballMVC/Models/Raptors.cs
@@ -7,18 +7,29 @@ namespace BasketballMVC.Models
 	{
 		public int Id { get; set; }
 
+		[Range(0,100)]
+		[Required]
 		[Display(Name = "Player Number")]
 		public int PlayerNum {  get; set; }
 
+		[StringLength(60,MinimumLength = 2)]
+		[Required]
 		[Display(Name = "Player Name")]
 		public string PlayerName { get; set; }
 
+        [StringLength(15, MinimumLength = 2)]
+        [Required]
 		[Display(Name = "Position")]
 		public string PlayerPosition { get; set; }
 
+		[Range(1,900000000)]
+		[DataType(DataType.Currency)]
+		[Required]
 		[Display(Name = "Salary")]
-		//[Column(TypeName ="double(18,2)")]
 		public double PlayerSalary {  get; set; }
+
+        [StringLength(60, MinimumLength = 2)]
+        [Required]
 		[Display(Name = "College")]
 		public string PlayerCollege { get; set; }
 	}
