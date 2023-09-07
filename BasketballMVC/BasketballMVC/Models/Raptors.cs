4fecbcf72976613d27432993a24aa2290169e480
GorkemKoksal <gorkem.asrin.koksal@gmail.com>
2023-09-07 16:06:28 +0300

    Headlines

--- BasketballMVC/BasketballMVC/Models/Raptors.cs
+++ BasketballMVC/BasketballMVC/Models/Raptors.cs
@@ -1,11 +1,23 @@
-﻿namespace BasketballMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BasketballMVC.Models
 {
 	public class Raptors
 	{
 		public int Id { get; set; }
+
+		[Display(Name = "Player Number")]
 		public int PlayerNum {  get; set; }
+
+		[Display(Name = "Player Name")]
 		public string PlayerName { get; set; }
+
+		[Display(Name = "Position")]
 		public string PlayerPosition { get; set; }
+
+		[Display(Name = "Salary")]
+		//[Column(TypeName ="double(18,2)")]
 		public double PlayerSalary {  get; set; }
 	}
 }
